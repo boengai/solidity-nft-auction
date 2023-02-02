7a53b4a9c3728dc6bf530d9b09a593534e2b5df2
boengai <keerati.khat@gmail.com>
2023-02-03 01:54:20 +0700

    ✨: bid method

--- src/0.8.17/NFTAuction.sol
+++ src/0.8.17/NFTAuction.sol
@@ -9,12 +9,18 @@ import "./interfaces/INFTAuction.sol";
 contract NFTAuction is ERC721Holder, INFTAuction {
     // errors
     error NFTAuction_CreateBadParameters();
+    error NFTAuction_BidBySeller();
+    error NFTAuction_BidRepeatly();
+    error NFTAuction_BidTooEarly();
+    error NFTAuction_BidTooLate();
+    error NFTAuction_BidTooLowThanMin();
+    error NFTAuction_BidLowThanHighest();
 
     // states
     uint256 private auctionIndex;
 
     struct Auction {
-        address seller;
+        address payable seller;
         address nftContract;
         uint256 nftTokenId;
         uint256 minBid;
@@ -23,7 +29,7 @@ contract NFTAuction is ERC721Holder, INFTAuction {
         bool isClaimed;
         uint256 highestBid;
         uint256 highestBidAt; // timestamp
-        address highestBidder;
+        address payable highestBidder;
     }
 
     mapping(uint256 => Auction) public auctions;
@@ -32,6 +38,7 @@ contract NFTAuction is ERC721Holder, INFTAuction {
     event CreateAuction(
         address seller, address nftContract, uint256 nftTokenId, uint256 minBid, uint256 startAt, uint256 endAt
     );
+    event BidAuction(uint256 auctionId, address bidder, uint256 bid, uint256 bidAt);
 
     constructor() {}
 
@@ -47,7 +54,7 @@ contract NFTAuction is ERC721Holder, INFTAuction {
 
         uint256 auctionId = ++auctionIndex;
 
-        auctions[auctionId].seller = msg.sender;
+        auctions[auctionId].seller = payable(msg.sender);
         auctions[auctionId].nftContract = address(_nftContract);
         auctions[auctionId].nftTokenId = _nftTokenId;
         auctions[auctionId].minBid = _minBid;
@@ -60,7 +67,44 @@ contract NFTAuction is ERC721Holder, INFTAuction {
         return auctionId;
     }
 
-    function bid(uint256 _auctionId) external payable {}
+    function bid(uint256 _auctionId) external payable {
+        Auction storage auction = auctions[_auctionId];
+
+        if (auction.seller == msg.sender) {
+            revert NFTAuction_BidBySeller();
+        }
+
+        if (auction.highestBidder == msg.sender) {
+            revert NFTAuction_BidRepeatly();
+        }
+
+        if (auction.startAt > block.timestamp) {
+            revert NFTAuction_BidTooEarly();
+        }
+
+        if (auction.endAt < block.timestamp) {
+            revert NFTAuction_BidTooLate();
+        }
+
+        if (msg.value < auction.highestBid) {
+            revert NFTAuction_BidLowThanHighest();
+        }
+
+        if ((msg.value - auction.highestBid) < auction.minBid) {
+            revert NFTAuction_BidTooLowThanMin();
+        }
+
+        // transfer previous bid back
+        if (auction.highestBidder != address(0)) {
+            auction.highestBidder.transfer(auction.highestBid);
+        }
+
+        auction.highestBid = msg.value;
+        auction.highestBidAt = block.timestamp;
+        auction.highestBidder = payable(msg.sender);
+
+        emit BidAuction(_auctionId, msg.sender, msg.value, block.timestamp);
+    }
 
     function claim(uint256 _auctionId) external {}
 }
