f5435adbac45df55b6a7c75c4f63f3a4d0a26480
boengai <keerati.khat@gmail.com>
2023-02-03 02:21:27 +0700

    ✅: claim unit test

---
+++ test/0.8.17/nft-auction/NFTAuction.claim.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.17;
+
+import "../../../src/0.8.17/NFTAuction.sol";
+import "../mocks/MockERC721.sol";
+import "../Base.t.sol";
+
+contract NFTAuctionBidTest is BaseTest {
+    MockERC721 public mockNFTA;
+    NFTAuction public nftAuction;
+    uint256 public aliceTokenId;
+    uint256 public aliceAuctionId;
+
+    function setUp() public {
+        payable(ALICE).transfer(100 ether);
+        payable(BOB).transfer(100 ether);
+        payable(CAROL).transfer(100 ether);
+        payable(DAN).transfer(100 ether);
+
+        mockNFTA = new MockERC721('NFT A', 'NFTA');
+        nftAuction = new NFTAuction();
+        aliceTokenId = mockNFTA.mintTo(ALICE);
+
+        vm.startPrank(ALICE);
+        mockNFTA.setApprovalForAll(address(nftAuction), true);
+        aliceAuctionId = nftAuction.create(mockNFTA, aliceTokenId, 0.5 ether, 3600, 84600);
+        vm.stopPrank();
+
+        skip(3601);
+
+        vm.startPrank(BOB);
+        nftAuction.bid{value: 0.6 ether}(aliceAuctionId);
+        vm.stopPrank();
+    }
+
+    function test_bid_ClaimAuction() public {
+        skip(84600);
+
+        vm.startPrank(BOB);
+        nftAuction.claim(aliceAuctionId);
+        vm.stopPrank();
+
+        // asset bid has been transfered
+        assertEq(ALICE.balance, 100.6 ether);
+        // asset NFT has been transfered
+        assertEq(mockNFTA.ownerOf(aliceTokenId), address(BOB));
+    }
+
+    function test_RevertWhenClaimTooEarly() public {
+        vm.startPrank(BOB);
+
+        vm.expectRevert(abi.encodeWithSignature("NFTAuction_ClaimTooEarly()"));
+        nftAuction.claim(aliceAuctionId);
+
+        vm.stopPrank();
+    }
+
+    function test_RevertWhenClaimNotHighestBidder() public {
+        skip(84600);
+
+        vm.startPrank(CAROL);
+
+        vm.expectRevert(abi.encodeWithSignature("NFTAuction_ClaimNotHighestBidder()"));
+        nftAuction.claim(aliceAuctionId);
+
+        vm.stopPrank();
+    }
+
+    function test_RevertWhenClaimDone() public {
+        skip(84600);
+
+        vm.startPrank(BOB);
+
+        nftAuction.claim(aliceAuctionId);
+
+        vm.expectRevert(abi.encodeWithSignature("NFTAuction_ClaimDone()"));
+        nftAuction.claim(aliceAuctionId);
+
+        vm.stopPrank();
+    }
+}
